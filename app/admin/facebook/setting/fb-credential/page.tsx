845d5e4bcadc78f5d45ee18591987c08db47459c
arjsabbir88 <arjsabbir88@gmail.com>
2026-01-10 19:52:31 +0600

    remove the find credential

--- app/admin/facebook/setting/fb-credential/page.tsx
+++ app/admin/facebook/setting/fb-credential/page.tsx
@@ -4,13 +4,6 @@ import { getFacebookPixelCredential } from "@/lib/facebook";
 
 const FacebookCredential = async() => {
 
-  const result = await getFacebookPixelCredential();
-
-
-
-  if(!result.success){
-    return <div className="min-w-full mx-auto">Failed to load Facebook credentials.</div>
-  }
 
   return (
     <div className="min-w-full mx-auto">
