ad31aae20f58330e05cf2287cff6496b83237ade
gmitsolution3 <gmitsolution3@gmail.com>
2026-02-15 12:11:29 +0600

    added google script

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,6 @@ export const metadata: Metadata = {
   description: "Build your trust",
 };
 
-
 export default async function RootLayout({
   children,
 }: Readonly<{
@@ -53,11 +52,33 @@ export default async function RootLayout({
             `,
           }}
         />
+        {/* <!-- Google Tag Manager --> */}
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NK6BMW6N');
+            `,
+          }}
+        />
       </head>
 
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NK6BMW6N"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
         <AuthProvider>
           <TimeTracker />
           <LocationTracker />
